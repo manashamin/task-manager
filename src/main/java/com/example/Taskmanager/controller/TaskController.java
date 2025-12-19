cf1ac269a271c2c2a707c25bc6c54666fadb0163
Manasha <23suca42@tcarts.in>
2025-12-19 09:37:46 +0530

    Initial commit - Task Manager project

---
+++ src/main/java/com/example/Taskmanager/controller/TaskController.java
@@ -0,0 +1,40 @@
+package com.example.Taskmanager.controller;
+
+import com.example.Taskmanager.model.Task;
+import com.example.Taskmanager.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+@RestController
+@RequestMapping("/tasks")
+
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "Task deleted successfully";
+    }
+}
